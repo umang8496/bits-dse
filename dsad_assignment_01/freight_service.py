0019ad85863cca1a15a7193ad160c71fa76b008e
umang8496 <umang8april@gmail.com>
2020-12-09 23:17:21 +0530

    Read the inputfile and store the information for further processing

--- dsad_assignment_01/freight_service.py
+++ dsad_assignment_01/freight_service.py
@@ -1,14 +1,22 @@
 # freight_service.py
+#####################################################################
 
-# Graph Node
+
+# Node Definition
 class Node(object):
     city_label = None
 
     def __init__(self, city_label):
         self.city_label = city_label
+    
+    def get_city_label(self):
+        return self.city_label
 
 
-# Graph Edge
+#####################################################################
+
+
+# Edge Definition
 class Edge(object):
     source = None
     target = None
@@ -29,27 +37,35 @@ class Edge(object):
         return self.train_label
 
 
+#####################################################################
+
+
 # Custom Graph Implementation
 class Graph(object):
     set_of_distinct_nodes = set()
     set_of_distinct_edges = set()
+    set_of_freight_trains = set()
+    set_of_distinct_cities = set()
     number_of_nodes = 0
     number_of_edges = 0
-    set_of_freight_trains = set()
-    # adjacency_matrix = []
-    adjacency_list = set()  
+    adjacency_matrix = []
 
-    def create_node(self, city):
-        node = Node(city)
-        self.number_of_nodes += 1
-        self.set_of_distinct_nodes.add(node)
-        return node
-    
-    def connect_nodes(self, node_A, node_B, train_id):
-        edge = Edge(node_A, node_B, train_id)
-        self.set_of_freight_trains.add(train_id)
-        self.number_of_edges += 1
-        self.set_of_distinct_edges.add(edge)
+    def __init__(self, number_of_distinct_cities, list_of_trains, list_of_routes):
+        self.number_of_distinct_cities = number_of_distinct_cities
+        self.list_of_trains = list_of_trains
+        self.list_of_routes = list_of_routes
+        self.initialize_adjacenct_matrix()
+
+    def initialize_adjacenct_matrix(self):
+        self.adjacency_matrix.append([0 for i in range(self.number_of_distinct_cities)])
+
+    def initializeGraph(self):
+        for i in range(len(self.list_of_routes)):
+            train_label = self.list_of_trains[i]
+            # create nodes out of the list of cities
+            list_of_cities = self.list_of_routes[i]
+            print(train_label,"<-->", list_of_cities)
+            # Work In Progress
 
     def get_number_of_nodes(self):
         return len(self.set_of_distinct_nodes)
@@ -58,69 +74,82 @@ class Graph(object):
         return len(self.set_of_freight_trains)
 
 
-# Utility Functions
-class Utility(object):
-    input_file = None
-    READ_MODE = None
+#####################################################################
 
+
+# FileUtilities Functions
+class FileUtilities(object):
     def __init__(self):
-        self.input_file = "inputPS22.txt"
-        self.prompt_file = "promptsPS22.txt"
         self.READ_MODE = "r"
+        self.list_of_trains = []
+        self.list_of_routes = []
+        self.set_of_distinct_cities = set()
 
-    def read_input_file(self):
-        file = open(self.input_file, self.READ_MODE)
+    def read_input_file(self, inputfile):
+        file = open(inputfile, self.READ_MODE)
         for line in file:
-            print(line)
+            entries = line.split("/")
+            stripped_entries = [item.strip() for item in entries]
+            self.list_of_trains.append(stripped_entries[0])
+            self.list_of_routes.append(stripped_entries[1:])
+            self.set_of_distinct_cities.update(set(stripped_entries[1:]))
         file.close()
 
-    def read_prompt_file(self):
-        file = open(self.prompt_file, self.READ_MODE)
-        for line in file:
-            print(line)
-        file.close()
+    def get_number_of_distinct_cities(self):
+        return len(self.set_of_distinct_cities)
 
-    def write_output_file(self):
+    def get_list_of_trains(self):
+        return self.list_of_trains
+
+    def get_list_of_routes(self):
+        return self.list_of_routes
+
+
+#####################################################################
+
+
+class FreightService(object):
+    def __init__(self):
+        self.graph = None
+        self.fileUtilities = FileUtilities()
+
+    def readCityTrainfile(self, inputfile):
+        # read the input file
+        self.fileUtilities.read_input_file(inputfile)
+        
+        # create the collections of trains and cities
+        number_of_distinct_cities = self.fileUtilities.get_number_of_distinct_cities()
+        list_of_trains = self.fileUtilities.get_list_of_trains()
+        list_of_routes = self.fileUtilities.get_list_of_routes()
+
+        # now create the Graph object and then initialize it
+        self.graph = Graph(number_of_distinct_cities, list_of_trains, list_of_routes)
+        self.graph.initializeGraph()
+
+    def showAll(self):
+        pass
+
+    def displayTransportHub(self):
+        pass
+
+    def displayConnectedCities(self, train):
+        pass
+
+    def displayDirectTrain(self, city_a, city_b):
+        pass
+
+    def findServiceAvailable(self, city_a, city_b): 
+        pass
+    
+    def readPromptFile(self, promptfile):
         pass
 
 
 #####################################################################
 if __name__ == "__main__":
-    # utility = Utility()
-    # utility.read_input_file()
-
-    g = Graph()
-
-    a = g.create_node("New Delhi")
-    b = g.create_node("Chennai")
-    train_id = "T1235"
-    g.connect_nodes(a, b, train_id)
-    c = g.create_node("Calcutta")
-    train_id = "T2342"
-    g.connect_nodes(a, c, train_id)
-
-    p = g.create_node("Vishakhapatnam")
-    q = g.create_node("Hyderabad")
-    train_id = "T5623"
-    g.connect_nodes(p, q, train_id)
-
-    l = g.create_node("ahmedabad")
-    m = g.create_node("Mumbai")
-    n = g.create_node("nagpur")
-    train_id = "T1122"
-    g.connect_nodes(l, m, train_id)
-    g.connect_nodes(m, n, train_id)
-    g.connect_nodes(l, n, train_id)
-
-    train_id = "T2341"
-    g.connect_nodes(a, l, train_id)
-    print("Number of Cities:", g.get_number_of_nodes())
-    print("Number of Trains:", g.get_number_of_freight_trains())
+    freightService = FreightService()
+    freightService.readCityTrainfile("inputPS22.txt")
+    # freightService.showAll()
 
 
-# Challenges
-# 1. For adjacency matrix the number of nodes is required in advance
-# 2. Reading the input file in correct format and then performing the instructions
-# 3. Creating an output file in the required format
-# 4. Cannot implement adjacency list as the dictionary data structure is not allowed
-
+    
