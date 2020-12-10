361fa5d56c7a4854e07e55e773e15e283e074ab7
umang8496 <umang8april@gmail.com>
2020-12-10 16:12:05 +0530

    Implement the logic for adjacency matrix for the freight-trains

--- dsad_assignment_01/freight_service.py
+++ dsad_assignment_01/freight_service.py
@@ -1,3 +1,4 @@
+#####################################################################
 # freight_service.py
 #####################################################################
 
@@ -42,37 +43,74 @@ class Edge(object):
 
 # Custom Graph Implementation
 class Graph(object):
-    set_of_distinct_nodes = set()
+    list_of_distinct_nodes = list()
+    list_of_distinct_cities = list()
     set_of_distinct_edges = set()
     set_of_freight_trains = set()
-    set_of_distinct_cities = set()
-    number_of_nodes = 0
-    number_of_edges = 0
     adjacency_matrix = []
 
-    def __init__(self, number_of_distinct_cities, list_of_trains, list_of_routes):
+    def __init__(self, number_of_distinct_cities, list_of_trains, list_of_routes, set_of_distinct_cities):
         self.number_of_distinct_cities = number_of_distinct_cities
         self.list_of_trains = list_of_trains
         self.list_of_routes = list_of_routes
+        self.set_of_distinct_cities = set_of_distinct_cities
         self.initialize_adjacenct_matrix()
 
     def initialize_adjacenct_matrix(self):
-        self.adjacency_matrix.append([0 for i in range(self.number_of_distinct_cities)])
+        self.adjacency_matrix = [[0] * self.number_of_distinct_cities for x in range(self.number_of_distinct_cities)]
+        self.adjacency_matrix = self.adjacency_matrix * self.number_of_distinct_cities
 
     def initializeGraph(self):
-        for i in range(len(self.list_of_routes)):
+        for city in self.set_of_distinct_cities:
+            self.list_of_distinct_cities.append(city)
+            self.list_of_distinct_nodes.append(self.create_node(city))
+
+        print(self.list_of_distinct_cities)
+
+        for i in range(len(self.list_of_trains)):
             train_label = self.list_of_trains[i]
             # create nodes out of the list of cities
             list_of_cities = self.list_of_routes[i]
-            print(train_label,"<-->", list_of_cities)
-            # Work In Progress
+            # print(train_label,"<-->", list_of_cities)
+            for f in range(len(list_of_cities)):
+                for g in range(f, len(list_of_cities)):
+                    source_city = list_of_cities[f]
+                    target_city = list_of_cities[g]
+                    if (source_city != target_city):
+                        source_index = self.list_of_distinct_cities.index(source_city)
+                        target_index = self.list_of_distinct_cities.index(target_city)
+                        source_node = self.list_of_distinct_nodes[source_index]
+                        target_node = self.list_of_distinct_nodes[target_index]
+                        edge = self.create_edge(source_node, target_node, train_label)
+                        self.update_adjacency_matrix(source_index, target_index)
+                        self.set_of_distinct_edges.add(edge)
+                    else:
+                        continue
 
-    def get_number_of_nodes(self):
-        return len(self.set_of_distinct_nodes)
+    def create_node(self, city_label):
+        return Node(city_label)
     
-    def get_number_of_freight_trains(self):
-        return len(self.set_of_freight_trains)
+    def create_edge(self, source, target, train_label):
+        return Edge(source, target, train_label)
 
+    def update_adjacency_matrix(self, index_of_source_city, index_of_target_city):
+        # print("indices:", index_of_source_city, index_of_target_city)
+        if (index_of_source_city != index_of_target_city):
+            self.adjacency_matrix[index_of_source_city][index_of_target_city] = 1
+            self.adjacency_matrix[index_of_target_city][index_of_source_city] = 1
+        else:
+            self.adjacency_matrix[index_of_source_city][index_of_target_city] = 0
+
+    def print_adjacency_matrix(self):
+        for i in range(self.number_of_distinct_cities):
+            for k in range(self.number_of_distinct_cities):
+                print((self.adjacency_matrix[i][k]), end="  ")
+            print()
+    
+    # remove the following function before submitting the assignment
+    def print_edge_list(self):
+        for edge in self.set_of_distinct_edges:
+            print(edge.train_label,":", edge.get_source().get_city_label(),"<--->", edge.get_target().get_city_label())
 
 #####################################################################
 
@@ -98,6 +136,9 @@ class FileUtilities(object):
     def get_number_of_distinct_cities(self):
         return len(self.set_of_distinct_cities)
 
+    def get_set_of_distinct_cities(self):
+        return self.set_of_distinct_cities
+
     def get_list_of_trains(self):
         return self.list_of_trains
 
@@ -121,10 +162,12 @@ class FreightService(object):
         number_of_distinct_cities = self.fileUtilities.get_number_of_distinct_cities()
         list_of_trains = self.fileUtilities.get_list_of_trains()
         list_of_routes = self.fileUtilities.get_list_of_routes()
+        set_of_distinct_cities = self.fileUtilities.get_set_of_distinct_cities()
 
         # now create the Graph object and then initialize it
-        self.graph = Graph(number_of_distinct_cities, list_of_trains, list_of_routes)
+        self.graph = Graph(number_of_distinct_cities, list_of_trains, list_of_routes, set_of_distinct_cities)
         self.graph.initializeGraph()
+        self.graph.print_adjacency_matrix()  # remove this function call
 
     def showAll(self):
         pass
@@ -152,4 +195,3 @@ if __name__ == "__main__":
     # freightService.showAll()
 
 
-    
